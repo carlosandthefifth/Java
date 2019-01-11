1447ae44c8babdcbf82aee83e36d13e89e920086
gcarloscodeview <gdansfdev@gmail.com>
2019-01-11 04:03:18 -0800

    comparetriplets doen

--- compareTheTriplets.java
+++ compareTheTriplets.java
@@ -1,47 +1,43 @@
 import java.util.*;
 
-public class  compareTheTriplets {
+public class compareTheTriplets {
 
-    public static int compareArray(int a, int b) {
-        if(a > b) return 1;
-        if(a < b) return 0;
-        return -1;
-    } 
+    // Complete the compareTriplets function below.
+    public static List<Integer> compareArray(List<Integer> a, List<Integer> b) {
 
-    public static void main(String[] args) {
-        List<Integer> anArray = new ArrayList<Integer>();
-        List<Integer> anArray2 = new ArrayList<Integer>();
+        List<Integer> Res = new ArrayList<Integer>();
         
-        int max = 100;
-        int min = 1; 
-
-       Random rand = new Random(max - min + 1);
-
-        for (int i = 0; i < 10; i++) {
-            anArray.add(rand.nextInt(max - min + 1));
-        }
-
-        for (int i = 0; i < 10; i++) {
-            anArray2.add(rand.nextInt(max - min + 1));
-        }
-
-        for (int i = 0; i <10; i++) {
-            System.out.println("a1["+i+"]: " + anArray.get(i));
-            System.out.println("a2["+i+"]: " + anArray2.get(i));
-        }
+        if (a.size() != b.size()) return Res;
 
         int Alice = 0;
         int Bob = 0;
-        
-        for (int ax = 0; ax < 10; ax++){
-            for (int bx = 0; bx < 10; bx++){
-                int result = compareArray(anArray.get(ax), anArray2.get(bx));
-                if (result==1) Alice++;
-                if (result==0) Bob++;
-            }
+        int ix = a.size();
+
+        for (int i = 0; i < ix; i++){
+            if (a.get(i) > b.get(i)) Alice++;
+            if (b.get(i) > a.get(i)) Bob++;
         }
-        System.out.print(Alice); 
-        System.out.print(" ");
-        System.out.println(Bob);
+        Res.add(Alice);
+        Res.add(Bob);
+
+        return Res;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList();
+        List<Integer> b = new ArrayList();
+        List<Integer> Res = new ArrayList();
+
+        a.add(5);
+        a.add(6);
+        a.add(7);
+        b.add(3);
+        b.add(6);
+        b.add(10);
+
+        Res = compareArray(a, b);
+        System.out.println("Res: " + Res);
+
+
     }
 }
